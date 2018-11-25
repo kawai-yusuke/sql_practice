93e141389ecd774cb29d52770305a7d440075c0f
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-25 14:35:19 +0900

    insertの練習

---
+++ sample02.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def main():
+    conn = sqlite3.connect("users.sqlite")
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO users(name,age) VALUES ('Tom', 22)"
+
+    cursor.execute(sql)
+
+    conn.commit()
+
+    conn.close()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
